fcc68c138cadefe71186f603fd250774117fc8a1
Linajjang <linakim@jejuesl.com>
2016-07-09 17:10:09 +0000

    Submitting a message(the form page)

--- app/actions.rb
+++ app/actions.rb
@@ -1,9 +1,19 @@
 # Homepage (Root path)
-get '/' do 
+get '/' do
   erb :index
 end
 
 get '/messages' do
   @messages = Message.all
-  erb :'Message/index'
+  erb :'messages/index'
+end
+
+post '/messages' do
+   @message = Message.new(
+	title: params[:title],
+	content: params[:content],
+	author: params[:author]
+   )
+   @message.save
+   redirect '/messages'
 end
